9df36c638fcdba35288de2c9c03ac78627c3d171
Andrew Samoilov <veprsag@gmail.com>
2022-01-19 16:03:39 +0200

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const makeDivItemMarkup = img => {
+    const { preview, original, description } = img;
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/></a>`;
+}
+
+const gall = document.querySelector('.gallery');
+const makeDivItemRows = galleryItems
+    .map(makeDivItemMarkup)
+    .join('');
+
+gall.insertAdjacentHTML("afterbegin", makeDivItemRows);
+
+gall.addEventListener("click", (event) => {
+    const isImg = event.target.classList.contains('gallery__image');
+    if (!isImg) return;
+    event.preventDefault();
+
+    console.log("Dataset.source ", event.target.dataset);
+
+    let lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+});
