473116d9618cbe3a4b49b8d600409f74a1a31984
Andrew Samoilov <veprsag@gmail.com>
2022-01-20 11:07:04 +0200

    Update 01-gallery.js
    * task - done !

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,5 @@
 import { galleryItems } from './gallery-items.js';
 
-// Change code below this line
-
 const makeDivItemMarkup = img => {
     const { preview, original, description } = img;
     return `<div class="gallery__item">
@@ -17,12 +15,25 @@ const makeDivItemRows = galleryItems
 gall.insertAdjacentHTML("afterbegin", makeDivItemRows);
 
 // console.table(galleryItems);
-
 // console.log(gall);
-gall.addEventListener("click", (event) => {
+
+const onGalleryClick = (event) => {
     const isImg = event.target.classList.contains('gallery__image');
+    console.log(`isImg  ${isImg}`);
     if (!isImg) return;
     event.preventDefault();
+
+    const onModalEscClose = (event) => {
+        console.log("Keydown: ", event.key);
+        if (event.key === 'Escape') {
+            console.log(`modalEscClose Escape`);
+            instance.close(() => {
+                document.removeEventListener("keydown", onModalEscClose);
+                console.log('lightbox not visible anymore');
+            });
+        }
+    };
+
 
     // console.log("Dataset.source ", event.target.dataset.source);
     const instance = basicLightbox.create(`
@@ -32,14 +43,35 @@ gall.addEventListener("click", (event) => {
 	`);
 
     instance.show();
+    document.addEventListener("keydown", onModalEscClose);
 
-    document.addEventListener("keydown", event => {
-        // console.log("Keydown: ", event);
-        if (event.key === 'Escape') {
-            console.log(`Escape Escape`);
-            document.removeEventListener("keydown", console.log(`Remove event listener`));
-            instance.close();
-        }
-    });
+}
 
-});
+
+gall.addEventListener("click", onGalleryClick);
+// gall.addEventListener("click", (event) => {
+//     const isImg = event.target.classList.contains('gallery__image');
+//     if (!isImg) return;
+//     event.preventDefault();
+
+//     // console.log("Dataset.source ", event.target.dataset.source);
+//     const instance = basicLightbox.create(`
+//     <div class="modal">
+//     <img src="${event.target.dataset.source}" width="100%" height="100%">
+//     </div>
+// 	`);
+
+//     instance.show();
+//     document.addEventListener("keydown", onModalEscClose());
+//     // document.addEventListener("keydown", ev => {
+//     //     // console.log("Keydown: ", event);
+//     //     if (ev.key === 'Escape') {
+//     //         // console.log(`Escape Escape`);
+//     //         instance.close(() => {
+//     //             document.removeEventListener("keydown", () => console.log(`Remove event listener`));
+//     //             console.log('lightbox not visible anymore');
+//     //         });
+//     //     }
+//     // });
+
+// });
